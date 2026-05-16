3fd8d662ab28ea282d01cdbcffdb4f491970ab5c
Trần Thanh Trọng <tt.trong.2108@gmail.com>
2026-05-16 23:51:46 +0700

    buoi 20

--- Buoi-20/js/script.js
+++ Buoi-20/js/script.js
@@ -28,4 +28,38 @@ Kết quả
 
 [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]*/
 var arr = [0, 1, [2, 3], [4, 5, [6, 7]], [8, [9, 10, [11, 12]]]];
-console.log(arr.flat(Infinity));
+var result = [];
+function flatten(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      flatten(arr[i]);
+    } else {
+      result.push(arr[i]);
+    }
+  }
+}
+flatten(arr);
+console.log(result);
+
+/*Bài 3
+    Tách phần tử trong mảng theo đúng kiểu dữ liệu
+
+    var arr3 = [["a", 1, true], ["b", 2, false]]
+
+
+    Kết quả
+
+    [["a", "b"], [1, 2], [true, false]]*/
+var arr3 = [
+  ["a", 1, true],
+  ["b", 2, false],
+];
+var res = [];
+for (var i = 0; i < arr3[0].length; i++) {
+  var group = [];
+  for (var j = 0; j < arr3.length; j++) {
+    group.push(arr3[j][i]);
+  }
+  res.push(group);
+}
+console.log(res);
